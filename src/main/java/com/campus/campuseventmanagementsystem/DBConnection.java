871a0896ace90e7a4b0b2af9d7758c0a6c4e46f0
priyankavijayakumar2008-ai <priyankavijayakumar2008@gmail.com>
2026-09-21 21:28:12 +0530

    Update DBConnection.java

--- src/main/java/com/campus/campuseventmanagementsystem/DBConnection.java
+++ src/main/java/com/campus/campuseventmanagementsystem/DBConnection.java
@@ -6,21 +6,35 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL =
-            "jdbc:mysql://localhost:3306/campus_event";
+    private static final String HOST =
+            System.getenv("DB_HOST");
 
-    private static final String USER = "root";
+    private static final String PORT =
+            System.getenv("DB_PORT");
+
+    private static final String DATABASE =
+            System.getenv("DB_NAME");
+
+    private static final String USER =
+            System.getenv("DB_USER");
 
     private static final String PASSWORD =
-            "Priya@2008";
+            System.getenv("DB_PASSWORD");
 
-   public static Connection getConnection() throws SQLException {
-    try {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-    } catch (ClassNotFoundException e) {
-        throw new SQLException("MySQL JDBC Driver not found!", e);
+    private static final String URL =
+            "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE
+            + "?ssl-mode=REQUIRED";
+
+    public static Connection getConnection() throws SQLException {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException(
+                    "MySQL JDBC Driver not found!", e);
+        }
+
+        return DriverManager.getConnection(
+                URL, USER, PASSWORD);
     }
-
-    return DriverManager.getConnection(URL, USER, PASSWORD);
-}
 }
